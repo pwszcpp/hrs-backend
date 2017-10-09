b21b64bce20309a74a92c5fe80870b750b506487
Michal Swierczek <pwszcpp@gmail.com>
2017-10-09 22:56:41 +0200

    first commit

---
+++ src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/HrsApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.oswiecim.pwsz.inf.hrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrsApplication.class, args);
+	}
+}
