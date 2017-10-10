2e95ba5bfb1abc211cf07ff0c56e7e93bb241efe
ghostd93 <p.kuba1993@gmail.com>
2017-10-10 20:21:54 +0200

    nie działa

--- src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/HrsApplication.java
+++ src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/HrsApplication.java
@@ -2,11 +2,25 @@ package pl.edu.oswiecim.pwsz.inf.hrs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.edu.oswiecim.pwsz.inf.hrs.model.Pracownik;
+import pl.edu.oswiecim.pwsz.inf.hrs.service.PracownikService;
 
 @SpringBootApplication
 public class HrsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrsApplication.class, args);
+
+		Pracownik p = new Pracownik();
+		p.setImie("Janusz");
+		p.setNazwisko("Tracz");
+		p.setPensja(5000);
+		p.setPlec("mezczyzna");
+		p.setWiek(43);
+		
+		PracownikService pracownikService = new PracownikService();
+
+		pracownikService.add(p);
+
 	}
 }
