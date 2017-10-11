779dd55a6be4682d04aefd3af10e88a12d97f9b4
ghostd93 <p.kuba1993@gmaul.com>
2017-10-11 18:48:31 +0200

    Dd

--- src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/HrsApplication.java
+++ src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/HrsApplication.java
@@ -1,15 +1,7 @@
 package pl.edu.oswiecim.pwsz.inf.hrs;
 
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
-import pl.edu.oswiecim.pwsz.inf.hrs.model.Pracownik;
-import pl.edu.oswiecim.pwsz.inf.hrs.service.PracownikServiceImpl;
-
-import javax.sql.DataSource;
-import java.util.List;
 
 @SpringBootApplication
 public class HrsApplication{
