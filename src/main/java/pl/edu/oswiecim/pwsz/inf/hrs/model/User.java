b7f184d9b26edde1372e570a41fad19a817f9b13
ghostd93 <p.kuba1993@gmaul.com>
2017-10-18 19:52:46 +0200

    training dodany

--- src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/model/User.java
+++ src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/model/User.java
@@ -1,6 +1,7 @@
 package pl.edu.oswiecim.pwsz.inf.hrs.model;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -20,7 +21,11 @@ public class User {
 
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles;
+    private Set<Role> roles = new HashSet<>();
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_training", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "training_id"))
+    private Set<Training> trainings = new HashSet<>();
 
     public User(){}
 
--- src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/repository/UserRepo.java
+++ src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/repository/UserRepo.java
@@ -1,8 +1,8 @@
 package pl.edu.oswiecim.pwsz.inf.hrs.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import pl.edu.oswiecim.pwsz.inf.hrs.model.User;
+        import org.springframework.data.repository.CrudRepository;
+        import org.springframework.stereotype.Repository;
+        import pl.edu.oswiecim.pwsz.inf.hrs.model.User;
 
 @Repository("userRepository")
 public interface UserRepo extends CrudRepository<User, Integer>{
