8fb2c3c6dc8daa3b67a5bba9dc261adf273d76ef
ghostd93 <p.kuba1993@gmail.com>
2017-10-24 20:24:03 +0200

    SpringConfig

---
+++ src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/config/SpringConfig.java
@@ -0,0 +1,13 @@
+package pl.edu.oswiecim.pwsz.inf.hrs.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
