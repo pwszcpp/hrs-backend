b37a5da6a71a1957d85d7a87ba04173cd84f79a1
ghostd93 <p.kuba1993@gmail.com>
2017-11-08 13:15:01 +0100

    Dodanie linków do listowania użytkowników szkolenia

--- src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/controller/TrainingController.java
+++ src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/controller/TrainingController.java
@@ -3,13 +3,10 @@ package pl.edu.oswiecim.pwsz.inf.hrs.controller;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
-import org.springframework.hateoas.mvc.ControllerLinkBuilder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.oswiecim.pwsz.inf.hrs.dto.TrainingDto;
 import pl.edu.oswiecim.pwsz.inf.hrs.model.Training;
@@ -22,8 +19,11 @@ import java.text.ParseException;
 import java.util.List;
 import java.util.Set;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
-@Controller
+
+@RestController
 @RequestMapping("/trainings")
 public class TrainingController {
 
@@ -73,8 +73,10 @@ public class TrainingController {
         List<TrainingDto> allTrainings = trainingService.findAllDTO();
         for(TrainingDto trainingDto : allTrainings){
             LOGGER.info("Training id: " + trainingDto.getTrainingId());
-            Link selfLink = ControllerLinkBuilder.linkTo(TrainingController.class).slash(trainingDto.getTrainingId()).withSelfRel();
+            Link selfLink = linkTo(TrainingController.class).slash(trainingDto.getTrainingId()).withSelfRel();
+            Link usersLink = linkTo(methodOn(TrainingController.class).getUsers(Integer.parseInt(trainingDto.getTrainingId()))).withRel("users");
             trainingDto.add(selfLink);
+            trainingDto.add(usersLink);
         }
         return allTrainings;
     }
@@ -82,14 +84,16 @@ public class TrainingController {
     @RequestMapping("/{id}")
     public @ResponseBody TrainingDto getTraining(@PathVariable("id") Integer id) {
         TrainingDto trainingDto = trainingService.convertToDTO(trainingService.findById(id));
-        Link selfLink = ControllerLinkBuilder.linkTo(TrainingController.class).slash(trainingDto.getTrainingId()).withSelfRel();
+        Link selfLink = linkTo(TrainingController.class).slash(trainingDto.getTrainingId()).withSelfRel();
+        Link usersLink = linkTo(methodOn(TrainingController.class).getUsers(id)).withRel("users");
         trainingDto.add(selfLink);
+        trainingDto.add(usersLink);
         return trainingDto;
     }
 
     @RequestMapping("/{id}/users")
     public @ResponseBody
-    Set<User> get(@PathVariable("id") Integer id) {
+    Set<User> getUsers(@PathVariable("id") Integer id) {
         LOGGER.info("Training Id: " + id );
         return trainingService.findById(id).getUsers();
 
