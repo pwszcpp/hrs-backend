6502e148f406d5b356feb6daf933bca07e6ecd5c
ghostd93 <p.kuba1993@gmail.com>
2017-11-20 18:13:07 +0100

    nie umiem

---
+++ src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/dto/UserDto.java
@@ -0,0 +1,86 @@
+package pl.edu.oswiecim.pwsz.inf.hrs.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import pl.edu.oswiecim.pwsz.inf.hrs.model.Role;
+
+import javax.annotation.Generated;
+import java.util.HashSet;
+import java.util.Set;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Generated("org.jsonschema2pojo")
+@JsonPropertyOrder({
+        "id",
+        "username",
+        "email",
+        "password"
+ //       "roles"
+})
+public class UserDto {
+    @JsonProperty("id")
+    private Integer id;
+
+    @JsonProperty("username")
+    private String username;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("password")
+    private String password;
+
+/*    @JsonProperty("roles")
+    private Set<Role> roles = new HashSet<>();*/
+
+    @JsonProperty("id")
+    public Integer getUserId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("username")
+    public String getUsername() {
+        return username;
+    }
+
+    @JsonProperty("username")
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @JsonProperty("email")
+    public String getEmail() {
+        return email;
+    }
+
+    @JsonProperty("email")
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonProperty("password")
+    public String getPassword() {
+        return password;
+    }
+
+    @JsonProperty("password")
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+/*    @JsonProperty("roles")
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    @JsonProperty("roles")
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }*/
+}
