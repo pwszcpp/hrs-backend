aae4f6abe9b659ded28dd55d08ac2c9c66f3703b
ghostd93 <p.kuba1993@gmail.com>
2017-12-07 11:16:45 +0100

    Remove cookie on logout

--- src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/config/CustomLogoutSuccessHandler.java
+++ src/main/java/pl/edu/oswiecim/pwsz/inf/hrs/config/CustomLogoutSuccessHandler.java
@@ -1,17 +1,16 @@
 package pl.edu.oswiecim.pwsz.inf.hrs.config;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Component
 public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
@@ -24,7 +23,11 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
                                 Authentication authentication) throws IOException, ServletException {
 
         LOGGER.info("LOGOUT");
-        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
 
+        Cookie cookie = new Cookie("Session", null);
+        cookie.setMaxAge(0);
+        httpServletResponse.addCookie(cookie);
+
+        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
     }
 }
