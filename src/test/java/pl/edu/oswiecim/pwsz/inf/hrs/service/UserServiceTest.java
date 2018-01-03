707dd26587b09ec7b58f78eb40f5d8a7e2cf8b51
WivenSarasvati <mateuszwojtas@interia.pl>
2018-01-03 17:36:46 +0100

    test xDD

--- src/test/java/pl/edu/oswiecim/pwsz/inf/hrs/service/UserServiceTest.java
+++ src/test/java/pl/edu/oswiecim/pwsz/inf/hrs/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package pl.edu.oswiecim.pwsz.inf.hrs.service;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,5 +41,29 @@ public class UserServiceTest {
         userService.saveUser(user);
         userService.saveUser(user);
 
+    }
+    @Test
+    public void findById_ReturnsNull_AfterDeleteUser() throws Exception {
+
+        User user = new User();
+        user.setUsername("user5");
+        user.setForename("user5");
+        user.setSurname("user5");
+        user.setEmail("user5@user.pl");
+        user.setPassword("user5");
+        user.setAddress("MW");
+        user.setEmploymentStartDate(new Date(2017,12,6));
+        user.setRole(46);
+        user.setStatus(true);
+        user.setPassExpire(new Date(2017,12,12));
+        user.setPassChangedDate(new Date(2017,12,12));
+        user.setLoginLastSuccess(new Date(2017,12,12));
+        user.setLoginLastFailed(new Date(2017,12,12));
+        user.setLoginAttemptsFailed(0);
+        userService.saveUser(user);
+        int temporaryId;
+        temporaryId = user.getId();
+        userService.deleteUser(temporaryId);
+        Assert.assertNull(userService.findById(temporaryId));
     }
 }
